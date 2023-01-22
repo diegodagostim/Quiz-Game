dfa6245d47bd22ec6c67a28701da91ad2b5f8450
diegodagostim <44005689+diegodagostim@users.noreply.github.com>
2023-01-22 09:13:24 +0000

    array of question and answers added to the project

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var quizQuestions = [
+  {
+    question: "What is the capital of France?",
+    answers: {
+      a: "Toulouse",
+      b: "Paris",
+      c: "Nice",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What is the main language in UK?",
+    answers: {
+      a: "Spanihs",
+      b: "French",
+      c: "English",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is color of the white horse of the King?",
+    answers: {
+      a: "Brown",
+      b: "White",
+      c: "Black",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "What are the colors of the japanese flag?",
+    answers: {
+      a: "Gray and White",
+      b: "White and Red",
+      c: "Red and Blue",
+    },
+    correctAnswer: "b",
+  },
+];
